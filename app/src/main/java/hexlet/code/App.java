9d69f865ffbf5225d33b810735a9da534f917739
prozet <abcd-xyz@list.ru>
2022-12-22 23:32:55 +0700

    st 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
+import hexlet.code.Cli;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Cli.greeting();
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        System.out.printf("Hello, %s!\n", scanner.next());
+    }
+}
