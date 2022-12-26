ee9cbccd5d7a2628f023d70f710dc9db4338bb2c
prozet <abcd-xyz@list.ru>
2022-12-26 23:06:22 +0700

    st 9

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,15 +11,7 @@ public class App {
     private static final int QUESTIONS_COUNT = 3;
 
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
-        System.out.print("Your choice: ");
+        showMenu();
         Scanner scanner = new Scanner(System.in);
         String userChoice = scanner.next();
         if (userChoice.equals("0")) {
@@ -38,6 +30,17 @@ public class App {
             case "6": Prime.run(userName, QUESTIONS_COUNT);
             default: return;
         }
+    }
 
+    private static void showMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
     }
 }
